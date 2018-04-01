e1a619e158c2056abd17180ae8d05b213b7ac292
olawale <olaclassik@gmail.com>
2018-04-01 22:14:12 +0100

    initial commit

---
+++ sample/wale.py
@@ -0,0 +1,14 @@
+import wx
+
+
+class wale(wx.Frame):
+
+    def __init__(self, parent, id):
+        wx.Frame.__init__(self, parent, id, 'Frame aka window', size=(300, 200))
+
+
+if __name__ == '__main__':
+    app = wx.PySimpleApp()
+    frame = wale(parent=None,id=-1)
+    frame.show()
+    app.MainLoop()
