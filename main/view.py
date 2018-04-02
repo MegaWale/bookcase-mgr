d354ee7337e44bca8f357e5c09f98f78f8c77499
olawale <olaclassik@gmail.com>
2018-04-02 18:30:49 +0100

    create simple menu bar

--- main/view.py
+++ main/view.py
@@ -7,6 +7,22 @@ class Frame(wx.Frame):
                 pos=wx.DefaultPosition,
                 title="BookCase Manager"):
         wx.Frame.__init__(self, parent, id, title, pos, size=(1100, 650))
+        panel = wx.Panel(self)
+
+        # Creating a status bar
+        status = self.CreateStatusBar()
+
+        # Creating a menu bar
+        menubar = wx.MenuBar()
+        first = wx.Menu()
+        second = wx.Menu()
+
+        first.Append(wx.NewId(), "New window", "This a New Window")
+        first.Append(wx.NewId(), "Open ...", "This a open wlnlksfjdk Window")
+
+        menubar.Append(first, "File")
+        menubar.Append(second, "Edit")
+        self.SetMenuBar(menubar)
 
 
 class App(wx.App):
